c938339673bf4657df64281f4dff72465415b41c
Julian <jsoares.cps@gmail.com>
2026-04-28 20:19:02 -0300

    terraform aula

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "Região da AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_id" {
+  description = "ID da VPC existente"
+  type        = string
+  default     = "vpc-0d3ffcde17772a7bb"
+}
+
+variable "subnet_id" {
+  description = "ID da Subnet existente"
+  type        = string
+  default     = "subnet-04a2e4f809cdb5738"
+}
